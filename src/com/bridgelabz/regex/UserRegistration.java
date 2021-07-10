f800fd053235433962a55eae53ae9971954fdd8d
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-10 18:56:35 +0530

    Added: initial setup for user registration using regex.

---
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.regex;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.print("Welcome to user registration problem using regex");
+    }
+
+}
